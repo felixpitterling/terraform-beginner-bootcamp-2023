2a394c13abcf8fd69b4a6bdab5d6854bc4921245
Felix Pitterling <f.pitterling@gmail.com>
2023-09-24 16:33:44 +0000

    #21 Restructure root module, add tagging to bucket & move to tf local

--- main.tf
+++ main.tf
@@ -1,36 +1,9 @@
-terraform {
-  cloud {
-    organization = "felixpitterling"
-
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.16.2"
-    }
-  }
-}
-
-provider "aws" {
-}
-provider "random" {
-  # Configuration options
-}
-
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
-  lower   = true
-  upper   = false
-  length  = 32
-  special = false
+  lower = true
+  upper = false
+  length   = 32
+  special  = false
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
@@ -38,8 +11,8 @@ resource "aws_s3_bucket" "example" {
   # Bucket Naming Rules
   #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
   bucket = random_string.bucket_name.result
-}
 
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
+  tags = {
+    UserUuid = var.user_uuid
+  }
 }
